33056edb3c650b39346f5ed206edf9932d110d71
ivanprofpv <akvabanka@gmail.com>
2023-05-06 23:04:37 +0400

    fix migrate for off email

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2023_05_05_155321) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: ""
     t.string "username", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -67,8 +67,8 @@ ActiveRecord::Schema.define(version: 2023_05_05_155321) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
